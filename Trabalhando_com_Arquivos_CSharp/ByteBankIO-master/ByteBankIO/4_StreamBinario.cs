ccbf16082d8fcf8e18a67f100158d5bd61bdfad9
Weslly Viana <devweslly@gmail.com>
2023-01-07 20:12:32 -0300

    Finalizando Projeto

---
+++ Trabalhando_com_Arquivos_CSharp/ByteBankIO-master/ByteBankIO/4_StreamBinario.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void EscritaBianaria()
+    {
+        using (var fs = new FileStream("contaCorrente.txt", FileMode.Create))
+        using (var escritor = new BinaryWriter(fs))
+        {
+            escritor.Write(456);        // Numero Agencia
+            escritor.Write(546544);     // Numero Conta
+            escritor.Write(4000.50);    // Saldo
+            escritor.Write("Gustavo Braga");
+        }
+    }
+
+    static void LeituraBinaria()
+    {
+        using (var fs = new FileStream("contaCorrente.txt", FileMode.Open))
+        using (var leitor = new BinaryReader(fs))
+        {
+            var agencia = leitor.ReadInt32();
+            var numeroConta = leitor.ReadInt32();
+            var saldo = leitor.ReadDouble();
+            var titular = leitor.ReadString();
+
+            Console.WriteLine($"{agencia}/{numeroConta} : {titular} : {saldo}");
+        }
+    }
+}
--- Trabalhando_com_Arquivos_CSharp/ByteBankIO-master/ByteBankIO/Program.cs
+++ Trabalhando_com_Arquivos_CSharp/ByteBankIO-master/ByteBankIO/Program.cs
@@ -1,12 +1,30 @@
 ﻿using ByteBankIO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 partial class Program
 {
     static void Main(string[] args)
     {
-        //  CriarArquivoComWriter();
-        TestaEscrita();
+        //Console.WriteLine("Digite o seu Nome:");
+        //var nome = Console.ReadLine();
+
+        var linhas = File.ReadAllLines("contas.txt");
+        Console.WriteLine(linhas.Length);
+
+        /*
+        foreach (var linha in linhas)
+        {
+            Console.WriteLine(linha);
+        }
+        */
+
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("EscrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
+        Console.WriteLine("Aplicação Finaliada!");
 
         Console.ReadLine();
     }
